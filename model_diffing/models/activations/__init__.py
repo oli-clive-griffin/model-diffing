700b5328c2382bdc03fd10a06294eb69c9822cef
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2025-03-11 16:17:53 +1300

    wip

--- model_diffing/models/activations/__init__.py
+++ model_diffing/models/activations/__init__.py
@@ -1,14 +1,16 @@
+from typing import cast
+
 from .activation_function import ActivationFunction
 from .jumprelu import AnthropicJumpReLUActivation
 from .relu import ReLUActivation
 from .topk import BatchTopkActivation, GroupMaxActivation, TopkActivation
 
 _classes: list[type[ActivationFunction]] = [
-    AnthropicJumpReLUActivation,
-    BatchTopkActivation,
-    GroupMaxActivation,
-    ReLUActivation,
-    TopkActivation,
+    cast(type[ActivationFunction], AnthropicJumpReLUActivation),
+    cast(type[ActivationFunction], BatchTopkActivation),
+    cast(type[ActivationFunction], GroupMaxActivation),
+    cast(type[ActivationFunction], ReLUActivation),
+    cast(type[ActivationFunction], TopkActivation),
 ]
 
 ACTIVATIONS_MAP: dict[str, type[ActivationFunction]] = {
