c39df535adb5a45a11ef53f9d8087901fd764db7
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2025-03-29 11:07:06 +1300

    remove sliding, nobias

--- model_diffing/scripts/train_jumprelu_sliding_window/config.py
+++
@@ -1,8 +0,0 @@
-from model_diffing.scripts.config_common import BaseExperimentConfig
-from model_diffing.scripts.train_jan_update_crosscoder.config import JanUpdateCrosscoderConfig, TanHSparsityTrainConfig
-
-
-class SlidingWindowExperimentConfig(BaseExperimentConfig):
-    crosscoder: JanUpdateCrosscoderConfig
-    train: TanHSparsityTrainConfig
-    hookpoints: list[str]
--- model_diffing/scripts/train_l1_sliding_window/config.py
+++
@@ -1,8 +0,0 @@
-from model_diffing.scripts.config_common import BaseExperimentConfig
-from model_diffing.scripts.train_l1_crosscoder.config import L1CrosscoderConfig, L1TrainConfig
-
-
-class L1SlidingWindowExperimentConfig(BaseExperimentConfig):
-    crosscoder: L1CrosscoderConfig
-    train: L1TrainConfig
-    hookpoints: list[str]
