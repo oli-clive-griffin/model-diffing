7eddf197bd22c01632c6ced398a68e077f87594f
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2025-04-01 15:12:45 +1300

    strip readme

--- README.md
+++ README.md
@@ -1,26 +1,3 @@
-# Model Diffing
+# Crosscode
 
-A library for training and analyzing SAEs, crosscoders, and transcoders.
-
-# Quickstart
-
-```bash
-python \
-    model_diffing/scripts/train_jan_update_crosscoder/run.py \
-    model_diffing/scripts/train_jan_update_crosscoder/example_config.yaml
-```
-
-# Development
-
-Suggested extensions and settings for VSCode are provided in `.vscode/`. To use the suggested
-settings, copy `.vscode/settings-example.json` to `.vscode/settings.json`.
-
-Development commands:
-
-```bash
-make check  # Run pre-commit on all files (i.e. pyright, ruff linter, and ruff formatter)
-make type  # Run pyright on all files
-make format  # Run ruff linter and formatter on all files
-make test  # Run tests that aren't marked `slow`
-make test-all  # Run all tests
-```
+A library for trainnig crosscoders, and by extension, transcoders, SAEs, and other sparse coding models.
--- crosscoding/trainers/skip_trans_crosscoder/trainer.py
+++ crosscoding/trainers/skip_trans_crosscoder/trainer.py
@@ -3,14 +3,14 @@
 
 # import torch
 
-# from model_diffing.models.activations.topk import TopkActivation
-# from model_diffing.models.crosscoder import ModelHookpointAcausalCrosscoder, InitStrategy
-# from model_diffing.scripts.base_trainer import BaseModelHookpointAcausalTrainer
-# from model_diffing.scripts.config_common import BaseTrainConfig
-# from model_diffing.utils import calculate_reconstruction_loss_summed_norm_MSEs, get_fvu_dict, random_direction_init_
+# from crosscoding.models.activations.topk import TopkActivation
+# from crosscoding.models.sparse_coders import InitStrategy, SAEOrTranscoder, ModelHookpointAcausalCrosscoder
+# from crosscoding.trainers.base_acausal_trainer import BaseModelHookpointAcausalTrainer
+# from crosscoding.trainers.config_common import BaseTrainConfig
+# from crosscoding.utils import calculate_reconstruction_loss_summed_norm_MSEs, get_fvu_dict, random_direction_init_
 
 
-# class ZeroDecSkipTranscoderInit(InitStrategy[ModelHookpointAcausalCrosscoder[TopkActivation]]):
+# class ZeroDecSkipTranscoderInit(InitStrategy[SAEOrTranscoder[TopkActivation]]):
 #     def __init__(
 #         self,
 #         activation_iterator_BMPD: Iterator[torch.Tensor],
@@ -22,20 +22,20 @@
 #         self.enc_init_norm = enc_init_norm
 
 #     @torch.no_grad()
-#     def init_weights(self, cc: ModelHookpointAcausalCrosscoder[TopkActivation]) -> None:
-#         random_direction_init_(cc.W_enc_XDL, self.enc_init_norm)
-#         cc.W_dec_LXoDo.zero_()
+#     def init_weights(self, cc: SAEOrTranscoder[TopkActivation]) -> None:
+#         random_direction_init_(cc.W_enc_DL, self.enc_init_norm)
+#         cc.W_dec_LD.zero_()
 
 #         assert cc.b_enc_L is not None, "This strategy expects an encoder bias"
 #         cc.b_enc_L.zero_()
 
-#         assert cc.b_dec_XD is not None, "This strategy expects a decoder bias"
-#         cc.b_dec_XD[:] = self._get_output_mean_MPD()
+#         assert cc.b_dec_D is not None, "This strategy expects a decoder bias"
+#         cc.b_dec_D[:] = self._get_output_mean_D()
 
-#         assert cc.W_skip_XDD is not None, "This strategy expects a skip connection"
-#         cc.W_skip_XDD.zero_()
+#         assert cc.W_skip_DD is not None, "This strategy expects a skip connection"
+#         cc.W_skip_DD.zero_()
 
-#     def _get_output_mean_MPD(self) -> torch.Tensor:
+#     def _get_output_mean_D(self) -> torch.Tensor:
 #         samples = []
 #         samples_processed = 0
 #         while samples_processed < self.n_samples_for_dec_mean:
@@ -46,30 +46,30 @@
 #             samples.append(output_BMPD)
 #             samples_processed += output_BMPD.shape[0]
 
-#         mean_samples_MPD = torch.cat(samples, dim=0).mean(0)
-#         return mean_samples_MPD
+#         mean_samples_D = torch.cat(samples, dim=0).mean(0)
+#         return mean_samples_D
 
 
-# class OrthogonalSkipTranscoderInit(InitStrategy[ModelHookpointAcausalCrosscoder[TopkActivation]]):
+# class OrthogonalSkipTranscoderInit(InitStrategy[SAEOrTranscoder[TopkActivation]]):
 #     def __init__(self, init_norm: float):
 #         self.init_norm = init_norm
 #         raise NotImplementedError("not confident in this implementation yet")
 
 #     @torch.no_grad()
-#     def init_weights(self, cc: ModelHookpointAcausalCrosscoder[TopkActivation]) -> None:
-#         random_direction_init_(cc.W_enc_XDL, self.init_norm)
+#     def init_weights(self, cc: SAEOrTranscoder[TopkActivation]) -> None:
+#         random_direction_init_(cc.W_enc_DL, self.init_norm)
 
-#         torch.nn.init.orthogonal_(cc.W_dec_LXoDo)
-#         cc.W_dec_LXoDo.mul_(self.init_norm)
+#         torch.nn.init.orthogonal_(cc.W_dec_LD)
+#         cc.W_dec_LD.mul_(self.init_norm)
 
 #         assert cc.b_enc_L is not None, "This strategy expects an encoder bias"
 #         cc.b_enc_L.zero_()
 
-#         assert cc.b_dec_XD is not None, "This strategy expects a decoder bias"
-#         cc.b_dec_XD.zero_()
+#         assert cc.b_dec_D is not None, "This strategy expects a decoder bias"
+#         cc.b_dec_D.zero_()
 
-#         assert cc.W_skip_XDD is not None, "This strategy expects a skip connection"
-#         cc.W_skip_XDD.zero_()
+#         assert cc.W_skip_DD is not None, "This strategy expects a skip connection"
+#         cc.W_skip_DD.zero_()
 
 
 # class TopkSkipTransCrosscoderTrainer(BaseModelHookpointAcausalTrainer[BaseTrainConfig, TopkActivation]):
@@ -89,21 +89,21 @@
 #         train_res: ModelHookpointAcausalCrosscoder.ForwardResult,
 #         log: bool,
 #     ) -> tuple[torch.Tensor, dict[str, float] | None]:
-#         assert batch_BMPD.shape[2] % 2 == 0, "we should have an even number of hookpoints for this trainer"
-#         batch_x_BMPiD = batch_BMPD[:, :, ::2]
-#         batch_y_BMPoD = batch_BMPD[:, :, 1::2]
+#         assert batch_BMPD.shape[1:-1] == (1, 2)
+#         batch_in_BD = batch_BMPD[:, 0, 0]
+#         batch_out_BD = batch_BMPD[:, 0, 1]
 
-#         train_res = self.crosscoder.forward_train(batch_x_BMPiD)
+#         train_res = self.crosscoder.forward_train(batch_in_BD)
 #         self.firing_tracker.add_batch(train_res.latents_BL)
 
-#         loss = calculate_reconstruction_loss_summed_norm_MSEs(train_res.recon_acts_BXD, batch_y_BMPoD)
+#         loss = calculate_reconstruction_loss_summed_norm_MSEs(train_res.output_BD, batch_out_BD)
 
 #         if log:
-#             assert batch_y_BMPoD.shape[2] == len(self.hookpoints[1::2])
-#             assert train_res.recon_acts_BXD.shape[2] == len(self.hookpoints[::2])
+#             assert batch_out_BPD.shape[2] == len(self.hookpoints[1::2])
+#             assert train_res.recon_acts_BMPD.shape[2] == len(self.hookpoints[::2])
 #             log_dict: dict[str, Any] = {
 #                 "train/loss": loss.item(),
-#                 **self._get_fvu_dict(batch_y_BMPoD, train_res.recon_acts_BXD),
+#                 **self._get_fvu_dict(batch_out_BPD, train_res.recon_acts_BMPD),
 #             }
 #             return loss, log_dict
 
